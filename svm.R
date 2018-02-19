4e0446154f78ff7a8ca6a7057364562b989bf88e
aishwaryashinde00 <33890896+aishwaryashinde00@users.noreply.github.com>
2018-02-19 10:10:09 +0530

    Add files via upload

---
+++ svm.R
@@ -0,0 +1,35 @@
+library(e1071)
+library("caret")
+library("caTools")
+setwd("~/")
+cancerData <- read.csv("wisc_bc_data.csv") 
+
+head(cancerData)
+str(cancerData)
+
+
+
+cancerData$group <- factor(cancerData$diagnosis)
+cancerData$group
+set.seed(1000)
+split = sample.split(cancerData$diagnosis, SplitRatio = 0.65)
+
+options("scipen"=999, "digits"=8)
+# Split up the data using subset
+train = subset(cancerData, split==TRUE)
+test = subset(cancerData, split==FALSE)
+testValues = test$diagnosis
+
+#Fit a model. The function syntax is very similar to lm function
+
+model_svm <- svm(diagnosis ~ .-id, train)
+
+#Use the predictions on the data
+
+pred <- predict(model_svm, test)
+
+
+#Plot the predictions and the plot to see our model fit
+
+table(pred, test$diagnosis)
+
